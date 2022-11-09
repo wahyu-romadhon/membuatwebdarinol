b00b8a84951c48291a8117dffcb1b684302224d0
wahyuromadhontrees <wahyu.romadhohn@treessolutions.com>
2022-11-09 21:45:09 +0700

    youtube-belajar-macam2-class-component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StateFullComponent from "./component/StateFullComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div style={{ textDecoration: "underline", fontSize: "30px" }}>
+          Belajar - Macam2 Class Component.
+        </div>
+        <br />
+        <div>1. StateFull Component</div>
+        <div>2. Pure Component</div>
+        <div>3. Stateless Component</div>
+      </div>
+      <hr />
+
+      <StateFullComponent />
+    </>
   );
 }
 
