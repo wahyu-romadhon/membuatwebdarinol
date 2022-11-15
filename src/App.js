ed64b383185a0cb293ddd39a5e5b18be911118d3
wahyuromadhontrees <wahyu.romadhohn@treessolutions.com>
2022-11-15 14:35:42 +0700

    youtube-template-v1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import "./App.css";
-import YoutubeComponent from "./component/YoutubeComponent";
+import Homepage from "./page/home";
 
 function App() {
   return (
     <>
-      <YoutubeComponent />
-      <div className="App">
-        <div
-          style={{
-            fontSize: "25px",
-            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-            border: 0,
-            borderRadius: 3,
-            boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-            color: "white",
-            padding: "15px",
-            cursor: "pointer",
-          }}
-        >
-          Belajar - Perbedaaan UseEffect & UseLayoutEffect
-        </div>
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        ></div>
-      </div>
+      <Homepage />
     </>
   );
 }
---
+++ src/page/home/index.js
@@ -0,0 +1,17 @@
+import "../../App.css";
+import YoutubeComponent from "../../component/YoutubeComponent";
+
+function Homepage() {
+  return (
+    <>
+      <YoutubeComponent />
+      <div className="App">
+        <div className="title-youtube ">
+          Belajar - Perbedaaan UseEffect & UseLayoutEffect
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
